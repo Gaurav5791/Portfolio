8ac35caee9740ab82dcc53309c883d7f9d1bcdee
Gaurav5791 <gauravgtp614@gmail.com>
2026-08-30 12:56:35 +0530

    Experiences change

--- backend/portfolio_project/settings.py
+++ backend/portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9x%o8xaop+)&nn@l!$b2=_mvl0)pzfyhb3(vv6++7_s&jpprsi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-api-ao2d.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
